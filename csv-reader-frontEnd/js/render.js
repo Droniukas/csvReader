f73748208a9643c6c4eefa50ca340b7a78f3c3b6
jokubasraila1 <jokubas.raila@roxit.nl>
2023-03-15 14:08:54 +0200

    some fixes

--- csv-reader-frontEnd/js/render.js
+++ csv-reader-frontEnd/js/render.js
@@ -1,79 +1,6 @@
 import { delRequest } from "./serverRequests.js";
 import { createTextArea } from "./editValues.js";
 
-var values = [
-  {
-    id: 1,
-    name: "domas",
-    email: "fdfd",
-    phoneNum: "fsdfsd",
-    dateCreated: "2023-03-12 17:59:26.74447",
-  },
-  {
-    id: 2,
-    name: "fsdfsd",
-    email: "fdfsd",
-    phoneNum: "fdfd",
-    dateCreated: "2023-03-12 17:59:26.824217",
-  },
-  {
-    id: 3,
-    name: "fsdfsd",
-    email: "fdfsd",
-    phoneNum: "fdfd",
-    dateCreated: "2023-03-12 17:59:26.827181",
-  },
-  {
-    id: 4,
-    name: "fsdfsd",
-    email: "fdfsd",
-    phoneNum: "fdfd",
-    dateCreated: "2023-03-12 17:59:26.829182",
-  },
-  {
-    id: 5,
-    name: "fsdfsd",
-    email: "fdfsd",
-    phoneNum: "fdfd",
-    dateCreated: "2023-03-12 17:59:26.832182",
-  },
-  {
-    id: 6,
-    name: "domasdomasdomasdomasdomas",
-    email: "fdfd@gmail.com",
-    phoneNum: "1234",
-    dateCreated: "2023-03-12 17:59:26.851166",
-  },
-  {
-    id: 7,
-    name: "petras",
-    email: "fdfd@gmail.com",
-    phoneNum: "1234",
-    dateCreated: "2023-03-12 17:59:26.855167",
-  },
-  {
-    id: 8,
-    name: "kelmis",
-    email: "fdfd@gmail.com",
-    phoneNum: "1234",
-    dateCreated: "2023-03-12 17:59:26.858168",
-  },
-  {
-    id: 9,
-    name: "vritis",
-    email: "fdfd@gmail.com",
-    phoneNum: "1234",
-    dateCreated: "2023-03-12 17:59:26.861168",
-  },
-  {
-    id: 10,
-    name: "joze",
-    email: "fdfd@gmail.com",
-    phoneNum: "1234",
-    dateCreated: "2023-03-12 17:59:26.865168",
-  },
-];
-
 function createDataCell(cellText, key, hasEditButton = false) {
   let columnItem = document.createElement("div");
   // columnItem.id = "columnItem" + key;
@@ -105,38 +32,42 @@ export function renderAll() {
   let inputBtn = document.getElementById("inputBtn");
   inputBtn.value = "";
 
-  // fetch("http://localhost:8080/api/independent")
-  // .then((res) => res.json())
-  // .then((contentsArr) => {
-  let contentsArr = values;
-  if (contentsArr.length == 0) {
-    mainContentDiv.innerHTML = "";
-    mainContentDiv.style.opacity = 0;
-    return;
-  }
-  mainContentDiv.innerHTML = "";
-  contentsArr.forEach((item) => {
-    mainContentDiv.style.opacity = 1;
-    let row = document.createElement("div");
-    row.className = "addedContentDiv";
+  fetch("http://localhost:8080/api/independent")
+    .then((res) => res.json())
+    .then((contentsArr) => {
+      if (contentsArr.length == 0) {
+        mainContentDiv.innerHTML = "";
+        mainContentDiv.style.opacity = 0;
+        return;
+      }
+      mainContentDiv.innerHTML = "";
+      contentsArr.forEach((item) => {
+        mainContentDiv.style.opacity = 1;
+        let row = document.createElement("div");
+        row.className = "addedContentDiv";
 
-    let singleDelBtn = createDeleteButton(item);
-    row.appendChild(singleDelBtn);
+        let singleDelBtn = createDeleteButton(item);
+        row.appendChild(singleDelBtn);
 
-    let name = createDataCell(item.name, "name", true);
-    row.appendChild(name);
+        let name = createDataCell(item.name, "name", true);
+        row.appendChild(name);
 
-    let email = createDataCell(item.email, "email", true);
-    row.appendChild(email);
+        let email = createDataCell(item.email, "email", true);
+        row.appendChild(email);
 
-    let phoneNum = createDataCell(item.phoneNum, "phoneNum", true);
-    row.appendChild(phoneNum);
+        let phoneNum = createDataCell(item.phoneNum, "phoneNum", true);
+        row.appendChild(phoneNum);
 
-    let dateCreated = createDataCell(item.dateCreated, "dateCreated", false);
-    row.appendChild(dateCreated);
+        let dateCreated = createDataCell(
+          item.dateCreated,
+          "dateCreated",
+          false
+        );
+        row.appendChild(dateCreated);
 
-    mainContentDiv.appendChild(row);
-  });
+        mainContentDiv.appendChild(row);
+      });
+    });
 }
 function createDeleteButton(item) {
   let singleDelBtn = document.createElement("button");
